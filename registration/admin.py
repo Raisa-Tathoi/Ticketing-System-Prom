5be7725f3e62479779cc6b9c7580f042055d93c7
Raisa Tathoi <tathoi.raisa@gmail.com>
2024-08-07 13:28:13 -0700

    changed the generation field to account for guest numbers only

--- registration/admin.py
+++ registration/admin.py
@@ -1,14 +1,22 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Booking, Guest
 
-class GuestInline(admin.TabularInline):
-    model = Guest
-    extra = 0
-    readonly_fields = ('name', 'email')
-
 class BookingAdmin(admin.ModelAdmin):
-    list_display = ('name', 'phone_number', 'school_email', 'num_tickets', 'alert_phone_number', 'pending_final')
-    inlines = [GuestInline]
+    list_display = ('booking_details', 'guests', 'alert_phone_number', 'pending_final')
+
+    def booking_details(self, obj):
+        return format_html('<strong>{}</strong><br>{}', obj.name, obj.school_email)
+
+    def guests(self, obj):
+        guests = obj.guests.all()
+        guest_details = "<br>".join(
+            [f"{guest.name} ({guest.email})" for guest in guests]
+        )
+        return format_html(guest_details)
+
+    booking_details.short_description = 'Booking Details'
+    guests.short_description = 'Guests'
 
 admin.site.register(Booking, BookingAdmin)
 admin.site.register(Guest)
