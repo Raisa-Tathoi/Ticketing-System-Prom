f77ce5cfaacf944b12d342d60835ceb09b7d7949
Raisa Tathoi <tathoi.raisa@gmail.com>
2024-08-08 21:42:15 -0700

    minor changes QR code now sends them to shawn's new song

--- registration/models.py
+++ registration/models.py
@@ -16,7 +16,7 @@ class Booking(models.Model):
     def save(self, *args, **kwargs):
         if not self.qr_code:
             qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
-            qr.add_data(f'http://yourdomain.com/booking/{self.id}')
+            qr.add_data(f'https://open.spotify.com/track/52A0oY4u5ufxrHC85JGf4x?si=c4ca3b97598e4f36')
             qr.make(fit=True)
             img = qr.make_image(fill='black', back_color='white')
             buffer = BytesIO()
